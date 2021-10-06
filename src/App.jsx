748dac0cb2e0aba7d6fa506e330e55ac5909242a
nazafiorentino <87139928+nazafiorentino@users.noreply.github.com>
2021-10-06 16:26:24 -0300

    Add files via upload
    cambioo2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+
+
 const App = () => (
   <>
     <Header />
     <Main />
+    
   </>
 );
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,32 @@
 import "./styles/Main.css";
+import Imagen from "./Imagen";
+import Masonry from 'react-masonry-css'
 const Main = () => {
-  return <main></main>;
+  return <main>
+    
+< Masonry 
+  breakpointCols = { 5 } 
+  className = "my-masonry-grid" 
+  columnClassName = "my-masonry-grid_column" > 
+
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+   <Imagen></Imagen>
+</Masonry >
+  </main>;
 };
 
 export default Main;
